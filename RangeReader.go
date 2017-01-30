6447474f4deaece63ea2ff7a0a2c5e6decf59bf8
rotemdan <rotemdan@gmail.com>
2017-01-30 11:39:40 +0200

    Simplified RangeReader code

--- RangeReader.go
+++ RangeReader.go
@@ -2,62 +2,33 @@ package main
 
 import (
 	"io"
-	//"log"
 )
 
 type RangeReader struct {
-	readerAt io.ReaderAt
-	offset   int64
-	endOffset int64
+	readerAt   io.ReaderAt
+	readOffset int64
+	endOffset  int64
 }
 
 func (this *RangeReader) Read(p []byte) (n int, err error) {
-	if this.endOffset >= 0 {
-		remainingByteCount := this.endOffset - this.offset
+	remainingByteCount := this.endOffset - this.readOffset
 
-		if remainingByteCount == 0 {
-			return 0, io.EOF
-		} else if remainingByteCount < int64(len(p)) {
-			p = p[0:remainingByteCount]
-		}
+	if remainingByteCount <= 0 {
+		return 0, io.EOF
+	} else if remainingByteCount < int64(len(p)) {
+		p = p[0:remainingByteCount]
 	}
 
-	n, err = this.readerAt.ReadAt(p, this.offset)
-	this.offset += int64(n)
+	n, err = this.readerAt.ReadAt(p, this.readOffset)
+	this.readOffset += int64(n)
 
 	return
 }
 
-/*
-func (this *RangeReader) ReadAt(p []byte, offset int64) (n int, err error) {
-	n, err = this.readerAt.ReadAt(p, offset)
-
-	if this.endOffset >= 0 {
-		remainingByteCount := this.endOffset - this.offset
-
-		if int64(n) > remainingByteCount {
-			if err == nil { 
-				return int(remainingByteCount), io.ErrUnexpectedEOF
-			} else {
-				return int(remainingByteCount), err
-			}
-		}
-	}
-
-	return
-}
-*/
-
-func NewRangeReader(readerAt io.ReaderAt, offset int64, endOffset int64) io.Reader {
+func NewRangeReader(readerAt io.ReaderAt, startOffset int64, endOffset int64) io.Reader {
 	return &RangeReader{
-		readerAt: readerAt,
-		offset:   offset,
-		endOffset: endOffset,
+		readerAt:   readerAt,
+		readOffset: startOffset,
+		endOffset:  endOffset,
 	}
 }
-/*
-type ReaderReaderAt interface {
-	io.Reader
-	io.ReaderAt
-}
-*/

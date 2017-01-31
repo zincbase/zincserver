e301ec6731b42c66bb5c78f64863fd58b3c4c361
rotemdan <rotemdan@gmail.com>
2017-01-31 12:29:37 +0200

    (Resolves #66) added unit tests for PrefetchingReaderAt

--- PrefetchingReaderAt.go
+++ PrefetchingReaderAt.go
@@ -16,11 +16,21 @@ type PrefetchingReaderAt struct {
 }
 
 func (this *PrefetchingReaderAt) ReadAt(p []byte, readOffset int64) (int, error) {
+	// Note that there is an implicit assumption here that subsequent calls to this
+	// function would have read offsets that are greater than the previous ones.
+	//
+	// If that isn't the case then the prefetcher would effectively preform like
+	// standard 'ReadAt()'.
+
 	readLength := len(p)
 
+	// If the requested range is within the prefetch buffer, copy a slice from the buffer
 	if readOffset >= this.prefetchStartOffset && readOffset+int64(readLength) <= this.prefetchEndOffset {
 		copy(p, this.prefetchBuffer[readOffset-this.prefetchStartOffset:])
 		return readLength, nil
+
+	// Otherwise if the requested size smaller than half the maximum prefetch buffer size,
+	// prefetch as much as possible and store the range, then return a copy of the data that was read
 	} else if readLength < MaxPrefetchSize/2 {
 		bytesPrefetched, err := this.readerAt.ReadAt(this.prefetchBuffer, readOffset)
 
@@ -34,6 +44,8 @@ func (this *PrefetchingReaderAt) ReadAt(p []byte, readOffset int64) (int, error)
 		bytesRead := MinInt(readLength, bytesPrefetched)
 		copy(p, this.prefetchBuffer[0:bytesRead])
 		return bytesRead, nil
+
+	// Otherwise just return the result of ReadAt
 	} else {
 		return this.readerAt.ReadAt(p, readOffset)
 	}
---
+++ PrefetchingReaderAt_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"math/rand"
+	//"log"
+)
+
+var _ = Describe("PrefetchingReaderAt", func() {
+	It("Reads from a short slice of bytes", func() {
+		randomBytes := []byte(RandomWordString(100))
+
+		reader := NewPrefetchingReaderAt(bytes.NewReader(randomBytes))
+
+		buf := make([]byte, 0)
+		n, err := reader.ReadAt(buf, 33)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(0))
+		Expect(buf).To(Equal([]byte{}))
+
+		buf = make([]byte, 1)
+		n, err = reader.ReadAt(buf, 93)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(1))
+		Expect(buf).To(Equal(randomBytes[93:94]))
+
+		buf = make([]byte, 3)
+		n, err = reader.ReadAt(buf, 11)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(3))
+		Expect(buf).To(Equal(randomBytes[11:14]))
+
+		buf = make([]byte, 13)
+		n, err = reader.ReadAt(buf, 95)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(5))
+		Expect(buf[0:n]).To(Equal(randomBytes[95:]))
+	})
+
+	It("Reads from a long (> 32768 characters) slice of bytes", func() {
+		randomBytes := []byte(RandomWordString(100000))
+		reader := NewPrefetchingReaderAt(bytes.NewReader(randomBytes))
+
+		//
+		buf := make([]byte, 1)
+		n, err := reader.ReadAt(buf, 33333)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(1))
+		Expect(buf).To(Equal(randomBytes[33333:33334]))
+
+		//
+		buf = make([]byte, 50000)
+		n, err = reader.ReadAt(buf, 20000)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(50000))
+		Expect(buf).To(Equal(randomBytes[20000 : 20000+50000]))
+
+		//
+		buf = make([]byte, 100000-2)
+		n, err = reader.ReadAt(buf, 2)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(100000 - 2))
+		Expect(buf).To(Equal(randomBytes[2:100000]))
+
+		//
+		buf = make([]byte, 10000)
+		n, err = reader.ReadAt(buf, 20000)
+
+		Expect(err).To(BeNil())
+		Expect(n).To(Equal(10000))
+		Expect(buf).To(Equal(randomBytes[20000 : 20000+10000]))
+	})
+
+	It("Reads random slices of random lengths from a long (> 32768 characters) slice of bytes", func() {	
+		randomBytes := []byte(RandomWordString(100000))
+		reader := NewPrefetchingReaderAt(bytes.NewReader(randomBytes))		
+
+		random := rand.New(rand.NewSource(0))
+
+		for i := 0; i < 100; i++ {
+			readOffset := random.Intn(100000)
+			readLength := random.Intn(100000 - readOffset)
+			
+			//log.Printf("Read offset: %d, Read length: %d", readOffset, readLength)
+
+			buf := make([]byte, readLength)
+			n, err := reader.ReadAt(buf, int64(readOffset))
+
+			Expect(err).To(BeNil())
+			Expect(n).To(Equal(readLength))
+			Expect(buf).To(Equal(randomBytes[readOffset : readOffset+readLength]))			
+		}
+	})
+})

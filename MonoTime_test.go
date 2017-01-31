0c4905c8cc379836e7d8724eeda98379d5688a1e
rotemdan <rotemdan@gmail.com>
2017-01-31 13:59:13 +0200

    Added tests for MonoTime

---
+++ MonoTime_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MonoTime", func() {
+	It("Returns a time within a one minute range of the system time", func() {
+		systemTimeUnixNano := time.Now().UnixNano()
+		const oneMinuteInNanoseconds = 60 * 1000000000
+
+		Expect(MonoUnixTimeNano()).To(BeNumerically(">", systemTimeUnixNano-oneMinuteInNanoseconds))
+		Expect(MonoUnixTimeNano()).To(BeNumerically("<", systemTimeUnixNano+oneMinuteInNanoseconds))
+
+		systemTimeUnixMicro := systemTimeUnixNano / 1000
+		const oneMinuteInMicroseconds = oneMinuteInNanoseconds / 1000
+		Expect(MonoUnixTimeMicro()).To(BeNumerically(">", systemTimeUnixMicro-oneMinuteInMicroseconds))
+		Expect(MonoUnixTimeMicro()).To(BeNumerically("<", systemTimeUnixMicro+oneMinuteInMicroseconds))
+
+		systemTimeUnixMilli := systemTimeUnixMicro / 1000
+		const oneMinuteInMilliseconds = oneMinuteInMicroseconds / 1000
+		Expect(MonoUnixTimeMilli()).To(BeNumerically(">", systemTimeUnixMilli-oneMinuteInMilliseconds))
+		Expect(MonoUnixTimeMilli()).To(BeNumerically("<", systemTimeUnixMilli+oneMinuteInMilliseconds))
+
+		Expect(MonoUnixTimeMilliFloat()).To(BeNumerically(">", systemTimeUnixMilli-oneMinuteInMilliseconds))
+		Expect(MonoUnixTimeMilliFloat()).To(BeNumerically("<", systemTimeUnixMilli+oneMinuteInMilliseconds))
+	})
+})
